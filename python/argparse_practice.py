dcdb98144335e13e750e8081c4c3dadcd096831a
Unan <unusualc@likelion.org>
2023-07-25 12:26:21 +0900

    Update argparse_practice.py

--- python/argparse_practice.py
+++ python/argparse_practice.py
@@ -19,3 +19,6 @@ parser2.add_argument("--brain", help="brain", action="store_true")
 args2 = parser.parse_args()
 if args2.brain:
     print("you are genius!")
+
+## References
+# https://supermemi.tistory.com/entry/%EB%A8%B8%EC%8B%A0-%EB%9F%AC%EB%8B%9D-%EB%AA%A8%EB%8D%B8%EC%97%90%EC%84%9C-argparse-%EC%82%AC%EC%9A%A9-%EB%B0%A9%EB%B2%95-%EC%98%88%EC%A0%9C
